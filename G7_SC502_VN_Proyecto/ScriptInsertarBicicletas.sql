62d750741999d12f7862280e22799505fbe7c948
Funnyd <dquesadaa2004@gmail.com>
2025-04-23 17:39:00 -0600

    Cambios menores de diseño y conexion

--- G7_SC502_VN_Proyecto/ScriptInsertarBicicletas.sql
+++
@@ -1,9 +0,0 @@
-use pura_cleta_cr;
-
-INSERT INTO bicicletas (nombre, descripcion, precio, imagen) VALUES
-('Bicicleta Montaña Pro', 'Perfecta para aventuras en terrenos difíciles. Suspensión completa y frenos de disco.', 225000, 'imagenes/bicicleta_montanera.jpg'),
-('Bicicleta Urbana Eco', 'Ligera y práctica para moverse por la ciudad. Incluye canasta frontal.', 135000, 'imagenes/bicicleta_urbana.jpg'),
-('Bicicleta Ruta Speedster', 'Diseñada para largas distancias y velocidad en carretera. Marco ligero de aluminio y cambios Shimano.', 275000, 'imagenes/bicicleta_ruta.jpg');
-
-INSERT INTO usuarios (nombre, email, password) VALUES
-('Lidieth Olivar', 'lolivar42@gmail.com', '$2y$10$ABC');
